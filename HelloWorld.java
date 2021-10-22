8e68263ca59db030a8e5cd20ba1c4d4f60d2fdb1
CoryKeuning <Cory.Keuning@gmail.com>
2021-10-22 09:24:59 -0500

    Created HelloWorld class and added some output lines.

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Git is Cool!");
+		System.out.println("Adding this on the new branch.");
+	}
+
+}
